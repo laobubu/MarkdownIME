1eea2e2bd136192cd8b42e3c9bfac84147068c96
laobubu <laobubu@gmail.com>
2016-03-09 15:02:53 +0800

    sematic

--- src/Renderer/Inline/InlineBracketRuleBase.ts
+++ src/Renderer/Inline/InlineBracketRuleBase.ts
@@ -6,23 +6,23 @@ namespace MarkdownIME.Renderer {
         name: string;
         tokens: string[];
 
-        abstract isLeftBracket(proc: InlineRenderProcess, token: IInlineToken): boolean;
-        abstract isRightBracket(proc: InlineRenderProcess, token: IInlineToken): boolean;
+        abstract isLeftBracket(proc: InlineRenderProcess, token: IInlineToken, tokenIndex?: number): boolean;
+        abstract isRightBracket(proc: InlineRenderProcess, token: IInlineToken, tokenIndex?: number): boolean;
         abstract ProcWrappedContent(proc: InlineRenderProcess, i1: number, i2: number);
 
         Proc(proc: InlineRenderProcess): boolean {
-            var si = proc.tokens[proc.stacki(1)];
-            var t = proc.tokens[proc.i];
+            var sti = proc.stacki(1), st = proc.tokens[sti];
+            var tti = proc.i, tt = proc.tokens[tti];
 
-            if (si.isToken && this.isLeftBracket(proc, si)) {
-                if (t.isToken && this.isRightBracket(proc, si)) {
+            if (st.isToken && this.isLeftBracket(proc, st, sti)) {
+                if (tt.isToken && this.isRightBracket(proc, tt, tti)) {
                     let i1 = proc.stacki(1), i2 = proc.i;
                     this.ProcWrappedContent(proc, i1, i2);
                     proc.popi();
 
                     return true;
                 }
-            } else if (t.isToken && this.isLeftBracket(proc, si)) {
+            } else if (tt.isToken && this.isLeftBracket(proc, tt, tti)) {
                 proc.pushi();
                 return true;
             }

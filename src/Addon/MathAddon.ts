06a32757cc827a1819067739ec440a34c2569bf0
laobubu <laobubu@gmail.com>
2016-03-10 00:26:09 +0800

    Lets have new MathAddon

--- src/Addon/MathAddon.ts
+++ src/Addon/MathAddon.ts
@@ -7,36 +7,49 @@ namespace MarkdownIME.Addon {
 	 * This addon MUST have a higher priority, than other inline elements like emphasising.
 	 * 
 	 * To enable, execute this:
-	 *  `MarkdownIME.Renderer.inlineRenderer.rules.unshift(new MarkdownIME.Addon.MathAddon())`
+	 *  `MarkdownIME.Renderer.inlineRenderer.addRule(new MarkdownIME.Addon.MathAddon())`
 	 * 
 	 * Use CODECOGS API to generate the picture.
 	 * @see http://latex.codecogs.com/eqneditor/editor.php
 	 * 
 	 * Originally planned to use http://www.mathjax.org/ , but failed due to its async proccessing.
 	 */
-	export class MathAddon implements MarkdownIME.Renderer.IInlineRule {
-
-		name = "MathFormula";
+	export class MathAddon implements MarkdownIME.Renderer.IInlineTokenRule {
 		
+		name = "MathFormula";
+
 		//this is the formula image URL prefix.
 		imgServer = 'http://latex.codecogs.com/gif.latex?';
-		
-		regex: RegExp = /([^\\]|^)(\${1,2})(.*?[^\\])\2/g;
-		
-		render(tree: DomChaos){
-			tree.replace(this.regex, (whole, leading, bracket, formula) => {
-				// var rtn = `<!--protect--><script type="math/tex">${wrapped}</script><!--/protect-->`;
-				var formulaHtmlized = Utils.text2html(formula);
-				var imgUrl = this.imgServer + encodeURIComponent(formula);
-				var rtn = `<!--protect--><!--formula:${formulaHtmlized}--><img alt="${formulaHtmlized}" class="formula" src="${imgUrl}"><!--/protect-->`
-				return leading + rtn;
-			});
-		}
-		
-		unrender(tree: DomChaos){
-			tree.screwUp(/<!--protect--><!--formula:(.+?)--><img .+?><!--\/protect-->/g, function (whole, formulaHtmlized) {
-				return '$' + Utils.html_entity_decode(formulaHtmlized) + '$';
-			})
+
+		tokens: string[] = ["$"];
+
+		Proc(proc: MarkdownIME.Renderer.InlineRenderProcess): boolean {
+			var i1 = proc.i, leftToken = proc.tokens[i1];
+			if (!proc.isToken(leftToken, this.tokens[0])) return false;
+
+			while (++proc.i < proc.tokens.length) {
+				var rightToken = proc.tokens[proc.i];
+				if (proc.isToken(rightToken, this.tokens[0])) {
+					if (proc.i === i1 + 1) {
+						// something like $$ , not valid
+						return false;
+					}
+
+					var img = proc.document.createElement('img');
+					var formula = proc.toString(proc.tokens.slice(i1 + 1, proc.i)).trim();
+					var imgUrl = this.imgServer + encodeURIComponent(formula);
+					img.setAttribute("src", imgUrl);
+					img.setAttribute("alt", formula);
+
+					proc.tokens.splice(i1, proc.i - i1 + 1, {
+						isToken: false,
+						data: img
+					});
+					return true;
+				}
+			}
+
+			return false;
 		}
 	}
 }

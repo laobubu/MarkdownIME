78e6ead5d1256434b16190357ec67158552ce395
laobubu <laobubu@gmail.com>
2016-03-09 23:13:00 +0800

    Fix EMPHASIS

--- src/Renderer/Inline/MarkdownRules.ts
+++ src/Renderer/Inline/MarkdownRules.ts
@@ -32,26 +32,58 @@ namespace MarkdownIME.Renderer {
                     return;
                 }
 
-                if (i2 === i1 + 2 && /^(EM|I)$/.test((<Node>proc.tokens[i1 + 1].data).nodeName)) {
-                    //something like `*<i>To Be Bold</i>*`
-                    proc.tokens.splice(i2, 1);
-                    proc.tokens.splice(i1, 1);
+                var innerTokens = proc.tokens.slice(i1 + 1, i2);
+                var tagName = this.tagNameEmphasis;
+                var document = proc.document;
 
-                    let srcElement = <Element>proc.tokens[i1].data;
-                    let newElement = proc.document.createElement(this.tagNameStrong);
-                    while (srcElement.firstChild) newElement.appendChild(srcElement.firstChild);
-                    proc.tokens[i1].data = newElement;
+                if (proc.isToken(proc.tokens[i1 - 1], this.tokens[0]) &&
+                    proc.isToken(proc.tokens[i2 + 1], this.tokens[0])) {
+                    /**
+                     * ## This is a `<strong>` tag
+                     * ```
+                     * The ***Fucking* one**
+                     *       >-------<         THIS IS CORRECT
+                     *     >>-------------<<   BUT THIS ONE!!
+                     * ```
+                     */
+                    tagName = this.tagNameStrong;
 
-                    proc.i -= 2;
+
+                    i1 -= 1;
+                    i2 += 1;
+                    proc.popi();
+                }
+                else if (innerTokens.some(item => {
+                    return !item.isToken && /^(EM|I)$/.test(item.data["nodeName"]);
+                })) {
+                    /**
+                     * ## Case 1: is a new start
+                     * ```
+                     * The ***Fucking* Fox *Jumps***
+                     *       >-------<         THIS IS CORRECT
+                     *      >--------------<   BUT WTF?! NOT A PAIR!
+                     * ```
+                     */
+                    if (innerTokens.length > 1) {
+                        proc.pushi();
+                        proc.pushi(); //one more stack push because of the following `proc.popi();`
+                    } else {
+                        proc.tokens.splice(i1, i2 - i1 + 1);
+                        let src = <Element>innerTokens[0].data;
+                        let dst = document.createElement(this.tagNameStrong);
+                        while (src.firstChild) dst.appendChild(src.firstChild);
+                        proc.tokens.splice(i1, 0, {
+                            isToken: false,
+                            data: dst
+                        });
+                    }
                     return;
                 }
 
-                var tokens = proc.tokens.splice(i1, i2 - i1 + 1);
-                tokens.pop();
-                tokens.shift();
+                proc.tokens.splice(i1, i2 - i1 + 1);
 
-                var fragment = proc.toFragment(tokens);
-                var UE = document.createElement(this.tagNameEmphasis);
+                var fragment = proc.toFragment(innerTokens);
+                var UE = document.createElement(tagName);
 
                 UE.appendChild(fragment);
                 proc.tokens.splice(i1, 0, {
@@ -80,6 +112,8 @@ namespace MarkdownIME.Renderer {
 
             ProcWrappedContent(proc: InlineRenderProcess, i1: number, i2: number) {
                 if (i2 === i1 + 1) return;
+
+                var document = proc.document;
 
                 var tokens = proc.tokens.splice(i1 - 1, i2 - i1 + 3);
                 tokens = tokens.slice(2, -2);

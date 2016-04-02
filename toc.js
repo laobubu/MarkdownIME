c09deee2bb17299e1b47ba66039abf853c824349
laobubu <laobubu@gmail.com>
2016-04-03 01:25:37 +0800

    Fix TOC Position

--- toc.js
+++ toc.js
@@ -1,21 +1,23 @@
 var $toc = $('#toc');
+var $tocTarget = $('.toc-source');
 
-(function makeTOC(parent) {
-	var titles = $(parent).children('h1,h2,h3,h4,h5');
-	titles.each(function(i,ele){
-		var anchor = document.createElement('a');
-		anchor.name = (ele.textContent);
-		ele.insertBefore(anchor, ele.firstChild);
-		
-		var link = document.createElement('a');
-		link.textContent = ele.textContent;
-		link.href = '#' + encodeURIComponent(anchor.name)
-		link.style.marginLeft = (1 * (ele.nodeName.substr(1) - 2)) + 'em';
-		
-		$toc.append(link);
-	})
-})('.toc-source');
+(function makeTOC() {
+    var titles = $tocTarget.children('h1,h2,h3,h4,h5');
+    titles.each(function(i, ele) {
+        var anchor = document.createElement('a');
+        anchor.name = (ele.textContent);
+        ele.insertBefore(anchor, ele.firstChild);
 
-$(window).scroll(function(){
-    $toc.css({top: Math.max(document.getElementById('s2').offsetTop-wst,0) + 'px'});
-})
+        var link = document.createElement('a');
+        link.textContent = ele.textContent;
+        link.href = '#' + encodeURIComponent(anchor.name)
+        link.style.marginLeft = (1 * (ele.nodeName.substr(1) - 2)) + 'em';
+
+        $toc.append(link);
+    })
+
+    var $window = $(window);
+    $window.scroll(function() {
+        $toc.css({ top: Math.max(parent.offset().top - $window.scrollTop(), 0) + 'px' });
+    })
+})();
